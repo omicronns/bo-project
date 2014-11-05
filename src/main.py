6a070f0e289945981b9e4e394c25b139b48e801b
omicronns <omicronns@gmail.com>
2014-11-05 20:42:26 +0100

    added goal function calculation inside init solution generator

--- src/main.py
+++ src/main.py
@@ -2,15 +2,23 @@
 from solver import Solver
 
 
-workPoints = 100
+workPoints = 50
 solver = Solver()
-solver.generateProblem(workPoints, 10, 5)
+solver.setToolsWorkload([2, 5, 7, 5, 5, 7, 5, 5, 7, 5])
+solver.generateProblem(workPoints, 2)
 print solver.generateInitSolution()
-a=[]
-for i in range(0, workPoints):
-    a.append(solver.getSolution().count(i))
-print solver.permuteSolution(1000)
-b=[]
-for i in range(0, workPoints):
-    b.append(solver.getSolution().count(i))
-print a==b
+print solver.getSolutionCost()
+print solver.generateInitSolution()
+print solver.getSolutionCost()
+print solver.generateInitSolution()
+print solver.getSolutionCost()
+print solver.generateInitSolution()
+print solver.getSolutionCost()
+# a=[]
+# for i in range(0, workPoints):
+#     a.append(solver.getSolution().count(i))
+# print solver.generateInitSolution()
+# b=[]
+# for i in range(0, workPoints):
+#     b.append(solver.getSolution().count(i))
+# print a==b
--- src/solver.py
+++ src/solver.py
@@ -1,5 +1,6 @@
 from random import randint
 from random import random
+from copy import deepcopy
 
 class Point(object):
     def __init__(self, x, y):
@@ -13,24 +14,40 @@ class Point(object):
     def rand():
         return Point(random(), random())
 
+    def copy(self):
+        return Point(self.x, self.y)
+
     def __str__(self):
         return "[" + str(self.x) + ", " + str(self.y) + "]"
 
 
 class Solver(object):
     __problem = []
+    __problemSize = 0
     __solution = []
+    __solutionLength = 0
+    __toolsWorkload = []
+    __toolsCount = 0
+    __cost = 0
 
     def setProblem(self, problem):
         self.__problem = problem
+        self.__problemSize = len(problem)
 
-    def getProblem(self, problem):
+    def setToolsWorkload(self, toolsWorkload):
+        self.__toolsWorkload = toolsWorkload
+        self.__toolsCount = len(toolsWorkload)
+
+    def getProblem(self):
         return self.__problem
 
     def getSolution(self):
         return self.__solution
 
-    def generateProblem(self, workPointsCount, toolsCount, maxToolsPerWorkPoint):
+    def getSolutionCost(self):
+        return self.__cost
+
+    def generateProblem(self, workPointsCount, maxStepsPerWorkPoint):
     # This function returns problem to solve in following format:
     # [[i, pt_i, [t_i0, t_i1, ... ,t_iN]], ...]
     # where:
@@ -39,33 +56,45 @@ class Solver(object):
     #   t_ik    is a k-th tool needed to use for i-th work point
         for workPointNumber in range(workPointsCount):
             point = Point.rand()
-            toolsAtThisWorkPoint = randint(1, maxToolsPerWorkPoint)
+            toolsAtThisWorkPoint = randint(1, maxStepsPerWorkPoint)
             taskTools = []
             for tool in range(toolsAtThisWorkPoint):
-                taskTools.append(randint(0, toolsCount))
+                taskTools.append(randint(0, self.__toolsCount - 1))
             workPoint = []
             workPoint.append(workPointNumber)
             workPoint.append(point)
             workPoint.append(taskTools)
             self.__problem.append(workPoint)
+        self.__problemSize = len(self.__problem)
         return self.__problem
 
+# TODO:Change this method so it would calculate goal function also
     def generateInitSolution(self):
     # Generates solution of form:
     # [k_1, k_2, ... , k_N]
     # where:
     #   k_i     is a number of work point needed to process in i-th step
-        while(len(self.__problem) != 0):
-            workPointToAddIndex = randint(0, len(self.__problem) - 1)
-            self.__problem[workPointToAddIndex][2].pop()
-            self.__solution.append(self.__problem[workPointToAddIndex][0])
-            if(len(self.__problem[workPointToAddIndex][2]) == 0):
-                del(self.__problem[workPointToAddIndex])
+        self.__cost = 0
+        self.__solution = []
+        previousPoint = Point(0,0)
+        problemCopy = deepcopy(self.__problem)
+        while(len(problemCopy) != 0):
+            workPointToAddIndex = randint(0, len(problemCopy) - 1)
+            # TODO: Make function count tool changes cost also
+            self.__cost += self.__toolsWorkload[problemCopy[workPointToAddIndex][2][0]]
+            self.__cost += previousPoint.distanceSqr(problemCopy[workPointToAddIndex][1])
+            previousPoint = problemCopy[workPointToAddIndex][1].copy();
+            del(problemCopy[workPointToAddIndex][2][0])
+            self.__solution.append(problemCopy[workPointToAddIndex][0])
+            if(len(problemCopy[workPointToAddIndex][2]) == 0):
+                del(problemCopy[workPointToAddIndex])
+        self.__solutionLength = len(self.__solution)
         return self.__solution
 
+# TODO:Change this method so it would change goal function also
     def permuteSolution(self, iterations):
-    # Permutes valid solution for iteration number of times
-        length = len(self.__solution)
+    # Permutes valid solution for iterations number of times
+        length = self.__solutionLength
         if(length == 0):
             return
         while(iterations > 0):
@@ -76,3 +105,4 @@ class Solver(object):
             self.__solution[j] = tmp
             iterations -= 1
         return self.__solution
+
